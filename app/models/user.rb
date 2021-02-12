2ea532b752d6e19d640e085050193cf87521de04
kuniyu <kuni10ura@gmail.com>
2021-02-12 13:38:59 +0900

    新規登録機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,14 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable,:validatable　
+
+  validates :nickname, presence: true
+  validates :last_name_kana, presence: true, format{with: /\A[\p{katakana --&&[^ -~.-°]]+\z/}
+  validates :first_name_kana, presence: true, format{with: /\A[\p{katakana --&&[^ -~.-°]]+\z/}
+  validates :first_name, presence: true, format:{with: /[^ -~.-°]+/}
+  validates :first_name, presence: true, format:{with: /[^ -~.-°]+/}
+
+
+
 end
