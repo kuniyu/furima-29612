5de6564067f0ddf7d26d5236cc086d6096d139e6
kuniyu <kuni10ura@gmail.com>
2021-02-16 19:04:13 +0900

    新規登録の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,16 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable,:validatable　
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :last_name_kana, presence: true, format{with: /\A[\p{katakana --&&[^ -~.-°]]+\z/}
-  validates :first_name_kana, presence: true, format{with: /\A[\p{katakana --&&[^ -~.-°]]+\z/}
-  validates :first_name, presence: true, format:{with: /[^ -~.-°]+/}
-  validates :first_name, presence: true, format:{with: /[^ -~.-°]+/}
+  validates :nickname, :birthday, presence: true
+  validates :last_name_kana, :first_name_kana, presence: true, 
+                                               format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'はカタカナで入力して下さい。' }
+  validates :password, :password_confirmation, presence: true, 
+                                               format: { with: /[a-z\d]{6,}/i, }
+
+  
+  
 
 
 
