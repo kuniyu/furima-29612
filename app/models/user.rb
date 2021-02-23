fe5e61fd04e75410e7fff28c2cc5e633d165ffee
kuniyu <kuni10ura@gmail.com>
2021-02-23 18:12:01 +0900

    バリデーションの訂正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,18 +3,33 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
 
-  validates :nickname, :birthday, presence: true
-  validates :last_name_kana, :first_name_kana, presence: true, 
-                                               format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'はカタカナで入力して下さい。' }
-  validates :password, :password_confirmation, presence: true, 
-                                               format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/}
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' } do
-    validates :first_name
+  with_options presence: true do
+    validates :nickname
+    validates :password
+    validates :password_confirmation
     validates :last_name
+    validates :first_name
+    validates :last_name_kana
+    validates :first_name_kana
+    validates :birthday
   end
   
+  with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' } do
+    validates :first_name
+    validates :last_name
+  end 
+
+  with_options format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'はカタカナで入力して下さい。' } do
+    validates :first_name_kana
+    validates :last_name_kana
+  end 
   
-
-
-
+  with_options format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/} do
+    validates :password
+    validates :password_confirmation
+  end
 end
+
+
+
+
