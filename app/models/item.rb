6753479f64ab62a8cb7e7a26bd1e8d741f43dab6
kuniyu <kuni10ura@gmail.com>
2021-03-04 18:31:35 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,10 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :deliverytime
+  belongs_to :prefecture
+  belongs_to :shipping_fee
+  belongs_to :condition
+  belongs_to :category
   belongs_to :user
   has_one_attached :image
 end
