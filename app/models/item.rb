a6f121c89e6fd0fe56e88cf764511de2185b0c37
kuniyu <kuni10ura@gmail.com>
2021-03-06 01:12:28 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,4 +7,17 @@ class Item < ApplicationRecord
   belongs_to :category
   belongs_to :user
   has_one_attached :image
+
+  with_options presence: true do
+    validates :product_name
+    validates :describe
+    validates :category_id
+    validates :condition_id
+    validates :shipping_fee_id
+    validates :prefectures_id
+    validates :delivery_time_id
+    validates :price
+  end
+
+  validates :price, format: { with: /^[0-9０-９]+$/ }
 end
