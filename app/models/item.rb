07b44f17821106902a70c71d2993d9f9227b00f5
kuniyu <kuni10ura@gmail.com>
2021-03-14 14:44:02 +0900

    バリデーションの訂正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,23 +1,30 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :deliverytime
-  belongs_to :prefecture
-  belongs_to :shipping_fee
-  belongs_to :condition
-  belongs_to :category
   belongs_to :user
   has_one_attached :image
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :shipping_fee
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :deliverytime
 
   with_options presence: true do
+    validates :image
     validates :product_name
     validates :describe
+    validates :price, inclusion: { in: 300..9_999_999 }, format: { with: /\A[0-9]+\z/ }
+  end
+
+  with_options numericality: { other_than: 0 } do
     validates :category_id
     validates :condition_id
     validates :shipping_fee_id
     validates :prefectures_id
     validates :delivery_time_id
-    validates :price
   end
 
-  validates :price, format: { with: /^[0-9０-９]+$/ }
+  validates :price, numericality: { only_integer: true }
+  
+
+  
 end
