d30854898ba3e8d0c294ee2d763cc79977c5ea01
kuniyu <kuni10ura@gmail.com>
2021-03-29 11:00:26 +0900

    指摘箇所の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
 
-  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :authenticate_user!, only: [:new, :edit, :update]
   before_action :set_item, only: [:edit, :show, :update]
   before_action :move_to_index, except: [:index, :show,]
 
@@ -48,12 +48,8 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    if user_signed_in? && current_user.id == @item.user_id
-      
-      
-    else user_signed_in? && current_user.id != @item.user_id
+    if user_signed_in? && current_user.id != @item.user_id
       redirect_to root_path
-      
     end
   end
 end
