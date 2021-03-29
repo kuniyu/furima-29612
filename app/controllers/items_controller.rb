78af73c8d55bdd422705ff8e9bc112e06c835a48
kuniyu <kuni10ura@gmail.com>
2021-03-29 11:26:46 +0900

    指摘箇所の修正２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
 
   before_action :authenticate_user!, only: [:new, :edit, :update]
   before_action :set_item, only: [:edit, :show, :update]
-  before_action :move_to_index, except: [:index, :show,]
+  before_action :move_to_index, except: [:index, :show,:new,:create]
 
   def index
     @items = Item.order("created_at DESC")
@@ -48,7 +48,7 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    if user_signed_in? && current_user.id != @item.user_id
+    if current_user.id != @item.user_id
       redirect_to root_path
     end
   end
