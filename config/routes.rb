78f9f3e53d72bd62be661bd1030e10df317f3c8d
kuniyu <kuni10ura@gmail.com>
2021-04-05 16:24:22 +0900

    ルーティングの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   
-  resources :items, only: [:index, :new, :create, :show, :edit, :update, :destroy]
-
+  resources :items
   root to: "items#index"
   
 end
