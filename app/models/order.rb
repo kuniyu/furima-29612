0d79c0d8467bc1b83edb7fa34a3bf0fe8bb5e3a0
kuniyu <kuni10ura@gmail.com>
2021-04-24 23:43:55 +0900

    バリデーションの設定(form_オブジェクト)

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,28 +2,23 @@ class Order
   include ActiveModel::Model
   attr_accessor :postal_code, :prefecture_id, :municipal, :block_number, 
   :building_number, :phone_number, :purchase_id, :user_id, :item_id, :token
-
+  VALID_PHONE_REGEX = /\A\d{10}$|^\d{11}\z/
 
   with_options presence: true do
-    validates :postal_code
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
     validates :prefecture_id
     validates :municipal
     validates :block_number
-    validates :phone_number
+    validates :phone_number, format: { with: VALID_PHONE_REGEX } 
     validates :user_id
     validates :item_id
-    validates :token
   end
 
-  validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/} do
-  end
-
-  validates :phone_number, format: {with: /\A[0-9]{3}-[0-9]{4}-[0-9]{4}\z/} do
-  end
+  
 
   def save
-    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, municipal: municipal, block_number: block_number, building_number: building_number, phone_number: phone_number)
-    Purchase.create(user_id: user_id, item_id: item_id)
+    purchase = Purchase.create(user_id: user_id, item_id: item_id)
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, municipal: municipal, block_number: block_number, building_number: building_number, phone_number: phone_number, purchase_id: purchase.id)
   end
  
 
