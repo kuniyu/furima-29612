09ed7bb5b9daed941cc5e2289fc14d53d5a3cfa5
kuniyu <kuni10ura@gmail.com>
2021-04-27 22:23:06 +0900

    before_actionの設定(2回目)

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,5 +1,7 @@
 class PurchasesController < ApplicationController
+  before_action :authenticate_user!, only: [:index, :create]
   before_action :set_item, only: [:index, :create] 
+  before_action :move_to_index, :index
   
    
   
@@ -36,5 +38,11 @@ class PurchasesController < ApplicationController
       card: order_params[:token],    # カードトークン
       currency: 'jpy'                 # 通貨の種類（日本円）
     )
+  end
+
+  def move_to_index
+    if current_user.id == @item.user_id || @item.purchase.present?
+      redirect_to root_path
+    end
   end
 end
