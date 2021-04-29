b9dc034a5d5837a8cae04b8c4efe25ddd332b0cb
kuniyu <kuni10ura@gmail.com>
2021-04-29 21:10:12 +0900

    before_actionの訂正

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,7 +1,7 @@
 class PurchasesController < ApplicationController
   before_action :authenticate_user!, only: [:index, :create]
   before_action :set_item, only: [:index, :create] 
-  before_action :move_to_index, :index
+  before_action :move_to_index, only: [:index, :create]
   
    
   
